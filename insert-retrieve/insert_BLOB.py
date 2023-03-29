6b8b23443bdefb3190c5554b087d1583c9332cf5
recoby <arocarivera@cifpfbmoll.eu>
2023-03-29 11:43:48 +0200

    Prueba del insert

--- insert-retrieve/insert_BLOB.py
+++ insert-retrieve/insert_BLOB.py
@@ -1,40 +1,45 @@
 import psycopg2
 
+# This Function will open & convert the image or file data to binary data.
+
+
 def convert_To_Binary(filename):
-    with open(filename, 'rb') as file:
-        data = file.read()
-    return data
+	with open(filename, 'rb') as file:
+		data = file.read()
+	return data
+
 
 def insert_BLOB(itemid, itemname, itemdescription, itemprice, FileName):
-    """ insert a BLOB into a table """
-    conn = None
-    try:
-        # connect to the PostgreSQL server & creating a cursor object
-        conn = psycopg2.connect(database ="Kebab", user = "alexr",
-                        password = "123456", host = "localhost", port = "5432")
+	""" insert a BLOB into a table """
+	conn = None
+	try:
 
-        # Creating a cursor with name cur.
-        cur = conn.cursor()
+		# connect to the PostgreSQL server & creating a cursor object
+		conn = psycopg2.connect(database ="Kebab", user = "postgres",
+                        password = "alex2004", host = "localhost", port = "5432")
 
-        # Binary Data
-        file_data = convert_To_Binary(FileName)
+		# Creating a cursor with name cur.
+		cur = conn.cursor()
 
-        # BLOB DataType
-        BLOB = psycopg2.Binary(file_data)
+		# Binary Data
+		file_data = convert_To_Binary(FileName)
 
-        # SQL query to insert data into the database.
-        cur.execute(
-            "INSERT INTO menuitems(itemid, itemname, itemdescription, itemprice, image) VALUES(%s,%s,%s,%s,%s)",
-            (itemid, itemname, itemdescription, itemprice, BLOB))
+		# BLOB DataType
+		BLOB = psycopg2.Binary(file_data)
 
-        # Close the connection
-        cur.close()
+		# SQL query to insert data into the database.
+		cur.execute(
+            "INSERT INTO menuitems(itemid, itemname, itemdescription, itemprice, image) VALUES(%s,%s,%s,%s,%s)",(itemid, itemname, itemdescription, itemprice, BLOB))
+		# Close the connection
+		cur.close()
 
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            # Commit the changes to the database
-            conn.commit()
+	except(Exception, psycopg2.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			# Commit the changes to the database
+			conn.commit()
+
 
 insert_BLOB(1, 'Kebab', 'An image of a kebab', 9.99, 'kebab.jpeg')
+
--- insert-retrieve/retrieve_BLOB.py
+++ insert-retrieve/retrieve_BLOB.py
@@ -1,45 +1,41 @@
+# Complete Function to Retrieve the BLOB data into the database.
 import psycopg2
 
-def binary_to_file(blob_data, new_file_name, old_file_name):
-    """Creates file from binary data"""
-    with open(new_file_name, 'wb') as file:
-        file.write(blob_data)
-    print(f"{old_file_name} file saved with name {new_file_name}")
-
-def retrieve_image(item_id, new_file_name):
-    """Retrieve an image from the menuitems table"""
-    conn = None
-    try:
-        # connect to the PostgreSQL server & creating a cursor object
-        conn = psycopg2.connect(
-            database="Kebab",
-            user="alexr",
-            password="123456",
-            host="localhost",
-            port="5432"
-        )
-
-        # Creating a cursor with name cur.
-        cur = conn.cursor()
-
-        # Retrieve image data from the database.
-        cur.execute('SELECT * FROM menuitems')
-        db = cur.fetchall()
-
-        blob_data = db[item_id-1][4]
-        binary_to_file(blob_data, new_file_name, db[item_id-1][1])
-
-        # Close the connection
-        cur.close()
-
-    except(Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            # Commit the changes to the database
-            conn.commit()
-            # Close the connection
-            conn.close()
+def Binary_To_File(BLOB, FileName, oldFileName):
+	"""Creates file from binary data"""
+	with open(f"{FileName}", 'wb') as file:
+		file.write(BLOB)
+	print(f"{oldFileName} File saved With Name name {FileName}")
 
 
-retrieve_image(1, 'kebab_from_db.jpeg')
+def retrieve_BLOB(itemid, FileName):
+	""" Retrieve a BLOB From a table """
+	conn = None
+	try:
+		# connect to the PostgreSQL server & creating a cursor object
+		conn = psycopg2.connect(database ="Kebab", user = "postgres",
+                        password = "alex2004", host = "localhost",
+                        port = "5432")
+
+		# Creating a cursor with name cur.
+		cur = conn.cursor()
+
+		# Retrieve BLOB data from the database.
+		cur.execute('SELECT * FROM menuitems')
+		db = cur.fetchall()
+
+		BLOB = db[itemid-1][2]
+		Binary_To_File(BLOB, FileName, db[itemid-1][1])
+
+		# Close the connection
+		cur.close()
+
+	except(Exception, psycopg2.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			# Commit the changes to the database
+			conn.commit()
+
+
+retrieve_BLOB(1, 'KebabFROMDB.png')
